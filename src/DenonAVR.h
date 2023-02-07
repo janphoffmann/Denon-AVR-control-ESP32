f50d945b674a45c65e96632473a094e46c406b4f
janphoffmann <59919531+janphoffmann@users.noreply.github.com>
2023-02-07 08:21:57 +0100

    Update DenonAVR.h

--- src/DenonAVR.h
+++ src/DenonAVR.h
@@ -1,72 +1,109 @@
 #ifndef DENONAVR_H
 #define DENONAVR_H
 
-#include "DenonVolume.h"
-
 #ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <HardwareSerial.h>
 #include <ESPmDNS.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <HardwareSerial.h>
-#include <ESPmDNS.h>
+#include "DenonVolume.h"
 #else
 #error Platform not supported
 #endif
 
+class DENON_AVR;
+class DenonProperties;
+
+
 typedef std::function<void(void*, AsyncClient*)> ConnHandler;
 typedef std::function<void(void*, AsyncClient*)> DisconnHandler;
-typedef std::function<void(const char *response, size_t len)> ResponseHandler;
-typedef std::function<void(const char *errorMessage)> ErrorHandler;
+typedef std::function<void(const char* response, size_t len)> ResponseHandler;
+typedef std::function<void(const char* errorMessage)> ErrorHandler;
+//neu eingefügt
+typedef std::function<void(void*, const char* state, size_t len, int nn)> StateHandler;
+typedef std::function<void(int i)> ResponseHandlerInt;
 
-typedef enum conType{RS232, TELNET};
+typedef enum IntOrChar { CHARACTER,
+                       INTEGER };
+//ende neu eingefügt
+typedef enum conType { RS232,
+                       TELNET };
 
-class DENON_AVR {
-  public:
-    DENON_AVR(){}
-    void _volume_feedback(const char *data,size_t len);
 
-    AsyncClient* AVClient;
-    HardwareSerial *_serialPort;
+class DenonProperties {
+  
+public:
 
-    DenonVolume Volume;
-    
-    bool begin(IPAddress _ip);
-    bool begin();
-    bool begin(HardwareSerial *serialPort);
+  DenonProperties(DENON_AVR* i);
+  DenonProperties(DENON_AVR* i, const char* Command, int j = _objNo);
+  DenonProperties(const char* Command, int j = _objNo);
 
-    void onConnect(ConnHandler callbackFunc);
-    void onDisconnect(DisconnHandler callbackFunc);
-    void onDenonResponse(ResponseHandler callbackFunc);
-    void onError(ErrorHandler callbackFunc);
+  static DENON_AVR* obj;
+  char* _Command;
+  void _feedback(const char* data, size_t len);
+  void addCommand(const char* Command);
+  void set(const char* i);
+  char* get();
+  int get(int i);
+  char is[30];
+  int objNo;
 
-    ConnHandler _connCallback = NULL;
-    DisconnHandler _disconnCallback = NULL;
-    ResponseHandler _denon_response_cb = NULL;
-    ErrorHandler _conErr_cb = NULL;
+  void onStateUpdate(ResponseHandler callbackFunc);
+  void onStateUpdate(ResponseHandlerInt callbackFunc);
+  ResponseHandler _state_update_cb = NULL;
+  ResponseHandlerInt _state_update_int_cb = NULL;
+  static int _objNo;
+};
 
-    bool set(const char* _command, const char *_value);
-    bool set(const char* _command, float i);
-    String get(const char* _command);
+class DENON_AVR : private DenonProperties{
+public:
+  DENON_AVR() : DenonProperties(this){}
+  void _volume_feedback(const char* data, size_t len);
 
-    bool write(const char* buf, size_t i){
-      if(AVClient->canSend() && AVClient->connected()){
-        AVClient->write(buf,i);
-        return true;
-      }
-      return false;
-    }      
-    void attachCb();
+  AsyncClient* AVClient;
+  HardwareSerial* _serialPort;
 
-    int stoi(String i) {
-      return int(i.toInt());
+  DenonVolume Volume;
+
+  bool begin(IPAddress _ip);
+  bool begin();
+  bool begin(HardwareSerial* serialPort);
+
+  void onConnect(ConnHandler callbackFunc);
+  void onDisconnect(DisconnHandler callbackFunc);
+  void onDenonResponse(ResponseHandler callbackFunc);
+  void onError(ErrorHandler callbackFunc);
+
+  ConnHandler _connCallback = NULL;
+  DisconnHandler _disconnCallback = NULL;
+  ResponseHandler _denon_response_cb = NULL;
+  ErrorHandler _conErr_cb = NULL;
+
+  bool set(const char* _command, const char* _value);
+  bool set(const char* _command, float i);
+  String get(const char* _command);
+
+  bool write(const char* buf, size_t i) {
+    if (AVClient->canSend() && AVClient->connected()) {
+      AVClient->write(buf, i);
+      return true;
     }
-  private:
-    IPAddress avr_ip;
-    conType _conType;    
+    return false;
+  }
+  void attachCb();
+
+  int stoi(String i) {
+    return int(i.toInt());
+  }
+
+  void onStateChange(StateHandler callbackFunc, void* arg = 0, int nn = 0);
+  StateHandler _SateCallback[10] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+  void* _StateCallback_arg = NULL;
+  int no[10];
+
+private:
+  IPAddress avr_ip;
+  conType _conType;
 };
 
 #endif
