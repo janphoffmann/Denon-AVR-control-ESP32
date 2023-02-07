01728b02691ce7a2311b989f3da109ce69f5e914
janphoffmann <59919531+janphoffmann@users.noreply.github.com>
2023-02-07 08:22:42 +0100

    Update DenonAVR.cpp

--- src/DenonAVR.cpp
+++ src/DenonAVR.cpp
@@ -60,8 +60,14 @@ void DENON_AVR::attachCb() {
     if (_connCallback != NULL) AVClient->onConnect(_connCallback, AVClient);
     if (_denon_response_cb != NULL) AVClient->onData([=](void *, AsyncClient *, void *data, size_t len) {
       _denon_response_cb((const char *)data, len);
-      this->_volume_feedback((const char *)data, len);
-    });
+      _volume_feedback((const char *)data, len);  //this-> entfernt !!!!!!!!!!!!!!!!!!!!
+      //neu eingefügt
+      for (int i = 0; i < 10; i++) {
+        if (_SateCallback[i] != 0) _SateCallback[i](_StateCallback_arg, (const char *)data, len, no[i]);
+      }
+      //ende neu einfügt
+    },
+                                                     this);  //this eingefügt!!!!!!!!!!!
     if (_disconnCallback != NULL) AVClient->onDisconnect(_disconnCallback, AVClient);
     if (_conErr_cb != NULL) AVClient->onError([=](void *, AsyncClient *, int8_t error) {
       _conErr_cb(AVClient->errorToString(error));
@@ -75,8 +81,15 @@ void DENON_AVR::attachCb() {
         uint8_t b[l];
         l = _serialPort->read(b, l);
         _denon_response_cb((const char *)b, l);
+        _volume_feedback((const char *)b, l);  //this-> entfernt !!!!!!!!!!!!!!!!
+        //neu eingefügt
+        for (int i = 0; i < 10; i++) {
+          if (_SateCallback[i] != 0) _SateCallback[i](_StateCallback_arg, (const char *)b,l, no[i]);
+        }
+        //ende neu einfügt
       }
-    });
+    },
+                                                           this);  //this eingefügt!!!!!!!!!
     if (_conErr_cb != NULL) _serialPort->onReceiveError([=](hardwareSerial_error_t error) {
       int err = error;
       if (err == 0) _conErr_cb("No Error");
@@ -136,6 +149,7 @@ bool DENON_AVR::set(const char *_command, float is) {
   return false;
 }
 
+
 //Callbacks for Telnet only
 void DENON_AVR::onConnect(ConnHandler callbackFunc) {
   _connCallback = callbackFunc;
@@ -150,3 +164,89 @@ void DENON_AVR::onDenonResponse(ResponseHandler callbackFunc) {
 void DENON_AVR::onError(ErrorHandler callbackFunc) {
   _conErr_cb = callbackFunc;
 }
+
+//neu eingefügt
+void DENON_AVR::onStateChange(StateHandler callbackFunc, void *arg, int nn) {
+  _SateCallback[nn] = callbackFunc;
+  _StateCallback_arg = arg;
+  no[nn] = nn;
+}
+
+void DenonProperties::_feedback(const char *data, size_t len) {
+  String in(data);
+  String input(in.substring(0, len));
+  String search(_Command);
+  if (input.indexOf(search) == 0 && len < 30) {
+    String sub(in.substring(search.length(), len));
+    const char* k = sub.c_str();
+    strncpy(is,k,sizeof(is));
+    if(_state_update_cb != 0) _state_update_cb((const char *)is, strlen(is));
+    if(_state_update_int_cb != 0) {
+      int i = sub.toInt();
+      _state_update_int_cb(i);
+    }
+  }
+}
+
+void DenonProperties::addCommand(const char *Command) {
+  size_t s = sizeof(Command);
+  _Command = new char[20];
+  strcpy(_Command,Command);
+  strcpy(is, "N/A");
+  obj->onStateChange([=](void *,const char *state,size_t len, int nn) -> void {  //war [&]!!!!!!!!!
+    _feedback(state,len);
+  },
+                     this, objNo);
+}
+
+// DenonProperties::DenonProperties(DENON_AVR *i, int j) {
+//   obj = i;
+//   obj->no[j] = j;
+//   objNo = j;
+// }
+
+int DenonProperties::_objNo = 0;
+DENON_AVR* DenonProperties::obj = NULL;
+
+DenonProperties::DenonProperties(DENON_AVR* i){
+  obj = i;
+}
+
+DenonProperties::DenonProperties(const char * Command, int j){
+  obj->no[j] = j;
+  objNo = j;
+  addCommand(Command);
+  _objNo++;
+}
+
+DenonProperties::DenonProperties(DENON_AVR* i, const char * Command, int j){
+  obj = i;
+  obj->no[j] = j;
+  objNo = j;
+  addCommand(Command);
+  _objNo++;
+}
+
+void DenonProperties::set(const char *i) {
+  obj->set(_Command, i);
+  strncpy(is, i,sizeof(is));
+  // is = (char *)i;
+}
+
+char *DenonProperties::get() {
+  return is;
+}
+int DenonProperties::get(int i){
+  String j(is);
+  if(_state_update_int_cb != 0) return j.toInt();
+  return -1;  
+}
+
+void DenonProperties::onStateUpdate(ResponseHandler callbackFunc){
+  _state_update_cb = callbackFunc;
+}
+
+void DenonProperties::onStateUpdate(ResponseHandlerInt callbackFunc){
+  _state_update_int_cb = callbackFunc;
+}
+//ende neu eingefügt
